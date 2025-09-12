4957dbe0af92e1d13b8625d8d322483d0294d637
Gabriel Batista <gabrielpb87@hotmail.com>
2025-09-13 00:36:15 +0100

    zenoh: code refactoring and improvements

--- uprotocol/zenoh-subscriber/src/publisher.rs
+++ uprotocol/zenoh-subscriber/src/publisher.rs
@@ -25,9 +25,17 @@ use up_rust::{UMessageBuilder, UPayloadFormat, UTransport, UUri};
 use up_transport_zenoh::UPTransportZenoh;
 use zenoh::config::{Config, EndPoint};
 
+// topic uURI -> up://hpc/A/2/8001
 const PUB_TOPIC_AUTHORITY: &str         = "hpc";
-const PUB_TOPIC_UE_ID: u32              = 0xB1DA;
-const PUB_TOPIC_UE_VERSION_MAJOR: u8    = 1;
+
+// topic uURI -> up://threadx/A/2/8001
+const PUB_TOPIC_AUTHORITY_2: &str       = "threadx";
+
+// topic uURI -> up://carla/A/2/8001
+// const PUB_TOPIC_AUTHORITY_3: &str       = "carla";
+
+const PUB_TOPIC_UE_ID: u32              = 0x000A;
+const PUB_TOPIC_UE_VERSION_MAJOR: u8    = 2;
 const PUB_TOPIC_RESOURCE_ID: u32        = 0x8001;
 
 #[tokio::main]
@@ -57,55 +65,73 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await
         .unwrap();
 
-    let resource_1 = PUB_TOPIC_RESOURCE_ID;
-    let authority_name_1 = PUB_TOPIC_AUTHORITY;
+    /*
+     * Possible combinations of different authorities and resource_ids to publish to
+     * |----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+     * |                                                                        pub source UUri definitions                                                                                     |
+     * |----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+     * | "up://PUB_TOPIC_AUTHORITY/A/2/PUB_TOPIC_RESOURCE_ID"   |   "up://PUB_TOPIC_AUTHORITY/A/2/(PUB_TOPIC_RESOURCE_ID + 1)"  |   "up://PUB_TOPIC_AUTHORITY/A/2/(PUB_TOPIC_RESOURCE_ID + 2)"  |
+     * | "up://PUB_TOPIC_AUTHORITY_2/A/2/PUB_TOPIC_RESOURCE_ID" |   "up://PUB_TOPIC_AUTHORITY_2/A/2/(PUB_TOPIC_RESOURCE_ID + 1)"|   "up://PUB_TOPIC_AUTHORITY_2/A/2/(PUB_TOPIC_RESOURCE_ID + 2)"|
+     * | "up://PUB_TOPIC_AUTHORITY_3/A/2/PUB_TOPIC_RESOURCE_ID" |   "up://PUB_TOPIC_AUTHORITY_3/A/2/(PUB_TOPIC_RESOURCE_ID + 1)"|   "up://PUB_TOPIC_AUTHORITY_3/A/2/(PUB_TOPIC_RESOURCE_ID + 2)"|
+     * |----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+     */
 
-    // One resource from multiple authorities
-    // let authority_names = [authority_name_1, "Vehicle_a", "sensor_hub"];
-    // let resource_ids = [resource_1, resource_1 + 1, resource_1 + 2];
-    
-    // Only one resource from one authority
-    let authority_names = [authority_name_1];
-    let resource_ids = [resource_1];
+    /*
+     * Placeholder to allow the definition of multiple authorities to publish to
+     */
+    // let authorities = [PUB_TOPIC_AUTHORITY, PUB_TOPIC_AUTHORITY_2, PUB_TOPIC_AUTHORITY_3];
+    // let authorities = [PUB_TOPIC_AUTHORITY, PUB_TOPIC_AUTHORITY_2];
+    let authorities = [PUB_TOPIC_AUTHORITY];
+
+    /*
+     * Placeholder to allow the definition of multiple resource_ids to publish to
+     */
+    // let resource_ids = [PUB_TOPIC_RESOURCE_ID, PUB_TOPIC_RESOURCE_ID + 1, PUB_TOPIC_RESOURCE_ID + 2];
+    // let resource_ids = [PUB_TOPIC_RESOURCE_ID, PUB_TOPIC_RESOURCE_ID + 1];
+    let resource_ids = [PUB_TOPIC_RESOURCE_ID];
 
     loop {
-        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+        for authority_name in authorities {
+            for resource_id in resource_ids {
+                let source = UUri {
+                    authority_name:     authority_name.to_string(),
+                    ue_id:              PUB_TOPIC_UE_ID,
+                    ue_version_major:   PUB_TOPIC_UE_VERSION_MAJOR as u32,
+                    resource_id:        resource_id,
+                    ..Default::default()
+                };
 
-        for (authority_name, resource_id) in authority_names.iter().zip(resource_ids.iter()) {
-            let source = UUri {
-                authority_name:     authority_name.to_string(),
-                ue_id:              PUB_TOPIC_UE_ID,
-                ue_version_major:   PUB_TOPIC_UE_VERSION_MAJOR as u32,
-                resource_id:        *resource_id,
-                ..Default::default()
-            };
+                let current_time = SystemTime::now()
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs();            
+                let payload_text = format!("Hello from '{}' - Resource: '0x{:X}' using {} - UTC: {current_time}", 
+                                        authority_name, resource_id, 
+                                        std::any::type_name_of_val(&client).split("::").last().unwrap_or("Unknown"));
 
-            let current_time = SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs();            
-            let payload_text = format!("Hello from '{}' - Resource: '0x{:X}' - UTC: {}", 
-                                                authority_name, resource_id, current_time);        
-            
-            let message = UMessageBuilder::publish(source.clone())
-                .with_ttl(1000)
-                .build_with_payload(
-                    payload_text.clone(),
-                    UPayloadFormat::UPAYLOAD_FORMAT_TEXT,
-                )
-                .expect("Failed to build message");
+                let message = UMessageBuilder::publish(source.clone())
+                    .with_ttl(1000)
+                    .build_with_payload(
+                        payload_text.clone(),
+                        UPayloadFormat::UPAYLOAD_FORMAT_TEXT,
+                    )
+                    .expect("Failed to build message");
 
-            if let Err(e) = client.send(message).await {
-                error!(
-                    "Failed to publish message payload: [{payload_text}] to source: [{}] : '{e}'",
-                    source.to_uri(true)
-                );
-            } else {
-                info!(
-                    "Successfully published message payload: [{payload_text}] to source: [{}]",
-                    source.to_uri(true)
-                );
+                if let Err(e) = client.send(message).await {
+                    error!(
+                        "Failed to publish message payload: [{payload_text}] to source: [{}] : '{e}'",
+                        source.to_uri(true)
+                    );
+                } else {
+                    info!(
+                        "Successfully published message payload: [{payload_text}] to source: [{}]",
+                        source.to_uri(true)
+                    );
+                }
             }
         }
+
+        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
     }
 }
