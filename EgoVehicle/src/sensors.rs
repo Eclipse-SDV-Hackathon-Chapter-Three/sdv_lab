8f8a7e67cd808252d9006595c74ffc75f65cd1c7
Pete LeVasseur <plevasseur@gmail.com>
2025-09-22 01:28:38 +0900

    Partial addition of listen mechanism for RadarMeasurement

--- EgoVehicle/src/sensors.rs
+++ EgoVehicle/src/sensors.rs
@@ -4,6 +4,7 @@ mod image;
 mod lane_invasion;
 mod obstacle_detection;
 mod sensor_comms;
+mod radar_measurement;
 
 pub use actor::*;
 pub use collision::*;
@@ -11,3 +12,4 @@ pub use image::*;
 pub use lane_invasion::*;
 pub use obstacle_detection::*;
 pub use sensor_comms::*;
+pub use radar_measurement::*;
